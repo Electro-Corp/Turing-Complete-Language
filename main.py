18ef6867a789c36cf581147e3c3bb3a8175afba4
Andrew Coon <andrew.coon84@gmail.com>
2022-09-21 20:12:01 -0600

    no work

--- main.py
+++ main.py
@@ -7,14 +7,27 @@ try:
     print("No file argument passed, running default file instead")
 except:
     filename = 'deftest.tc'
-VARLEN = 2400
-vars = [[None]*VARLEN]*VARLEN
+VARLEN = 2
+FUNCLEN = 2
+CLASSLEN = 2
 
+# [loc]
+varnames = [[""]*VARLEN]*VARLEN
+vars = [0]*VARLEN
 
+funcs = [[0]*VARLEN]*VARLEN
+funcnames = [""]*VARLEN
 
+classes = [[0]*VARLEN]*VARLEN
+classnames = [[""]*VARLEN]*VARLEN
 
+varnames[0][1] = "Works"
+varnames[1] = "I think"
+
+print(varnames)
 
 # if commands
+v = 0
 def parsecommands(commands):
     i = 0
     for i in range(len(commands)):
@@ -22,7 +35,13 @@ def parsecommands(commands):
         syntax = command[0]
         arguments = command[1:]
         print(arguments)
-
+        if(syntax == 'var'):
+            v+=1
+            varnames[v] = arguments[0]
+            if(arguments[1] == '='):
+                varnames[v][0] = arguments[2]
+            vars[v] = 1
+            print('var found')
 try:
     with open(str(filename), 'r') as d:
         lines = d.readlines()
@@ -32,7 +51,7 @@ except FileNotFoundError:
 print("File found! Running..")
 for e in range(len(lines)):
     lines[e] = lines[e].strip("\n")
-try:
-    parsecommands(lines)
-except Exception as exc:
-   print("error lol")
+#try:
+parsecommands(lines)
+#except Exception as exc:
+#   print("error lol")
